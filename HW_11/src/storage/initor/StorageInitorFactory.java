98f9cbbd77a9df728ca3e1ca11836723c15ff419
HocKu7 <pupkinvasya7000@gmail.com>
2020-01-10 02:44:22 +0300

    11

---
+++ HW_11/src/storage/initor/StorageInitorFactory.java
@@ -0,0 +1,29 @@
+package storage.initor;
+
+import storage.initor.fileInitor.InTextFileStorageInitor;
+import storage.initor.fileInitor.XmlDomFileDataInitor;
+
+public class StorageInitorFactory {
+    private StorageInitorFactory(){
+
+    }
+
+    public static StorageInitor getStorageInitor(InitStorageType initStorageType){
+        switch (initStorageType){
+
+            case MEMORY: {
+                return new InMemoryStorageInitor();
+            }
+            case TEXT_FILE: {
+                return new InTextFileStorageInitor();
+            }
+            case XML_FILE:{
+                return new XmlDomFileDataInitor();
+            }
+            default:{
+                throw new RuntimeException("Unknown storage init type " + initStorageType);
+            }
+        }
+    }
+
+}
