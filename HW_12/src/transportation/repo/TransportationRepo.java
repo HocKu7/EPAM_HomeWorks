1e7c0077bb9792f500aae08934bda49699afa266
HocKu7 <pupkinvasya7000@gmail.com>
2020-01-13 01:00:16 +0300

    SAX parser

---
+++ HW_12/src/transportation/repo/TransportationRepo.java
@@ -0,0 +1,7 @@
+package transportation.repo;
+
+import transportation.domain.Transportation;
+import common.business.repo.CommonRepo;
+
+public interface TransportationRepo extends CommonRepo<Transportation, Long> {
+}
